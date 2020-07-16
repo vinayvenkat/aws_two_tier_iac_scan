57ef627e5a1793fd1d5eecf80b40cdf2e646c179
vinayvenkat <vinayvenkat@yahoo.com>
2020-07-15 22:39:04 -0700

    Setup the demo environment

--- aws_two_tier.tf
+++ aws_two_tier.tf
@@ -213,7 +213,7 @@ resource "aws_security_group" "sgWideOpen" {
     from_port   = "0"
     to_port     = "0"
     protocol    = "-1"
-    cidr_blocks = ["192.168.0.0/16"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
