9b0fcae140744f9e9b337e8e45d1a3a8ec25cf66
vinayvenkat <vinayvenkat@yahoo.com>
2020-08-26 15:32:57 -0700

    Updating the S3 bucket configs

--- s3-buckets.tf
+++ s3-buckets.tf
@@ -2,8 +2,8 @@ resource "aws_s3_bucket" "foo" {
   bucket        = "tf-test-trail"
   force_destroy = true
 
-  acl    = "private"
-  versioning {
-    enabled = true
-  }
+  acl    = "public"
+  #versioning {
+  #  enabled = true
+  #}
 }
