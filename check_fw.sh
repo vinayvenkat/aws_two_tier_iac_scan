cb336774164e1d0426cfcc1997bd2d380ad097ba
vinayvenkat <vinayvenkat@yahoo.com>
2020-10-27 21:44:42 -0700

    push new fixes and updates

--- check_fw.sh
+++ check_fw.sh
@@ -1,4 +1,5 @@
 # This piece of code tests to ensure that the firewall is up and ready to accept API traffic on the mgmt port.
+# This piece of code tests to ensure that the firewall is up and ready to accept API traffic on the mgmt port.
 while true
 do
   echo "$1" >> /tmp/pan.log
